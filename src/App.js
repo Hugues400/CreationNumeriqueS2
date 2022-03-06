1b58ab282fa124afd5e8df4c9ad7bd62a8b2d6a8
Hugues400 <hugues.puiseux@edu.ece.fr>
2022-03-06 18:10:34 +0100

    commit
    c

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+   render() {
+     return (
+         <div>
+           <h1>Bonjour</h1>
+           <h1 style={{textAlign:"left"}}>Hello</h1>
+         </div>
+     );
+   }
+}
+export default App;
+

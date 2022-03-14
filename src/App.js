86786342a370117862ddd884914bcc0a4d327562
Hugues400 <hugues.puiseux@edu.ece.fr>
2022-03-14 15:45:26 +0100

    élément de base du projet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Alert } from "react-alert";
 
+import Footer from "./Footer";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,10 +10,14 @@ class App extends React.Component {
      return (
          <div>
            <h1>Bonjour</h1>
-           <h1 style={{textAlign:"left"}}>Hello</h1>
+           <h1 className="titre1">Hello</h1>
+
+           <div>
+             <Footer/>
+           </div>
          </div>
      );
-   }
+  }
 }
 export default App;
 
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Footer extends React.Component {
+   render() {
+     return (
+         <div className="div-footer">
+           bonjour
+         </div>
+     );
+   }
+}
+export default Footer;
+

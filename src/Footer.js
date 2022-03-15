dc9b6ff2b2f53485251d1ca20a32d54941d65a2f
Hugues400 <hugues.puiseux@edu.ece.fr>
2022-03-15 08:39:55 +0100

    mise en place dossier site html

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+// Assuming logo.png is in the same folder as JS file
+ import logo from './image_logo.png';
+ 
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,9 +9,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Footer extends React.Component {
    render() {
      return (
-         <div className="div-footer">
-           bonjour
-         </div>
+         <div className="div_footer">
+
+            <div className="container">
+              <div className="row">
+                <div className="col-4">
+                  <div className="div_logo">
+                    <img src={logo} className="image_logo" alt="logo"/>
+                  </div>
+                </div>
+                <div className="col-8">
+                  <div className="div_information">
+                    bonjur
+                  </div>
+                </div>
+              </div>
+             </div>
+
+          </div>
      );
    }
 }
